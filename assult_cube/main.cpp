09bb2bfa15dbef9f7bde5e90a1c45035fa01216d
Guy Krinsky <guykrinsky9@gmail.com>
2021-07-31 14:02:11 +0300

    Add project files.

---
+++ assult_cube/main.cpp
@@ -0,0 +1,234 @@
+#include <Windows.h>
+#include "stdio.h"
+#include <Psapi.h>
+#include <processthreadsapi.h>
+
+LPCWSTR WINDOW_NAME = L"AssaultCube";
+
+#define PLAYER_RELATIVE_ADDRESS 0x10F4F4
+#define HEALTH_OFFSET 0xF8
+
+#define FUNC_LENGTH 549 // In bytes.
+
+DWORD getProcBaseAdd(HANDLE processHandle);
+
+int main()
+{
+    DWORD pId = 0;
+	HWND hWnd = FindWindow(0, WINDOW_NAME);
+	if (hWnd == NULL)
+	{
+		printf("error 1");
+		return 0;
+	}
+    GetWindowThreadProcessId(hWnd, &pId);
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pId);
+    if (!hProc)
+    {
+        perror("Open process failed");
+        printf("error 2");
+        return 0;
+    }
+
+    // Write function to proc
+    PVOID memAddr = (PVOID)VirtualAllocEx(hProc, NULL, FUNC_LENGTH, MEM_COMMIT, PAGE_READWRITE);
+
+
+    // get proc handle
+    
+
+    // Find reciol func
+
+    // Switch bites to call mine function
+    return 1;
+}
+
+void increaseHealth()
+{
+    // If you change this function you have to change FUNC_LENGTH
+    // By using "nm -S --size-sort -t d <objfile>" command.
+
+    DWORD playerHealth = 0;
+    // Get proc Id
+    DWORD pId = GetCurrentProcessId();
+    // get proc handle
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pId);
+    if (!hProc)
+    {
+        perror("Open process failed");
+        printf("error 2");
+        return;
+    }
+
+    // proc baseaddress after lodaing to the RAM. probably 0x400000.
+    DWORD baseAddress = (DWORD)GetModuleHandle(NULL);
+    printf("proc start in this address: %d \n", baseAddress);
+
+    // The pointer to player struct is a static address.
+    // always be in the same relative place to proc base address
+    DWORD_PTR pPlayer = baseAddress + PLAYER_RELATIVE_ADDRESS;
+    printf("Player address is: %d \n", pPlayer);
+
+    // Get player health location
+    PVOID pHealth = 0;
+    int is_valid = ReadProcessMemory(hProc, (LPCVOID)pPlayer, &pHealth, sizeof(DWORD), NULL);
+    if (!is_valid)
+    {
+        printf("error 3");
+        return;
+    }
+    // pHealth now got the address of the player.
+    pHealth = PVOID(DWORD(pHealth) + HEALTH_OFFSET);
+
+    // Get player health.
+    printf("player health address is: %d \n", (DWORD)pHealth);
+    is_valid = ReadProcessMemory(hProc, pHealth, &playerHealth, sizeof(int), NULL);
+    if (!is_valid)
+    {
+        printf("error 4");
+        return;
+    }
+    printf("Player current health is %d \n", playerHealth);
+
+    DWORD newPlayerHealth = playerHealth + 1;
+    //Change player health.
+    WriteProcessMemory(hProc, pHealth, &newPlayerHealth, sizeof(int), NULL);
+}
+
+DWORD_PTR getProcBaseAdd(HANDLE processHandle)
+{
+    // TODO: undresatnd code.
+    DWORD_PTR   baseAddress = 0;
+    HMODULE* moduleArray;
+    LPBYTE      moduleArrayBytes;
+    DWORD       bytesRequired;
+
+    if (EnumProcessModules(processHandle, NULL, 0, &bytesRequired))
+    {
+        if (bytesRequired)
+        {
+            moduleArrayBytes = (LPBYTE)LocalAlloc(LPTR, bytesRequired);
+
+            if (moduleArrayBytes)
+            {
+                unsigned int moduleCount;
+
+                moduleCount = bytesRequired / sizeof(HMODULE);
+                moduleArray = (HMODULE*)moduleArrayBytes;
+
+                if (EnumProcessModules(processHandle, moduleArray, bytesRequired, &bytesRequired))
+                {
+                    baseAddress = (DWORD_PTR)moduleArray[0];
+                }
+
+                LocalFree(moduleArrayBytes);
+            }
+        }
+    }
+    return baseAddress;
+}
+
+
+
+
+//#include <Windows.h>
+//#include "stdio.h"
+//#include <Psapi.h>
+//
+//LPCWSTR WINDOW_NAME = L"AssaultCube";
+//DWORD NEW_PLAYER_HEALTH = 10000;
+//
+//#define PLAYER_RELATIVE_ADDRESS 0x10F4F4
+//#define HEALTH_OFFSET 0xF8
+//
+//DWORD getModuleHandle(HANDLE processHandle);
+//
+//int main()
+//{
+//  DWORD playerHealth = 0;
+//    // Get proc Id.
+//	HWND hWnd = FindWindow(0, WINDOW_NAME);
+//	if (hWnd == NULL)
+//	{
+//		printf("error 1");
+//		return 0;
+//	}
+//	DWORD pId;
+//	GetWindowThreadProcessId(hWnd, &pId);
+//    
+//    // get proc handle
+//	HANDLE hProc= OpenProcess(PROCESS_ALL_ACCESS, FALSE, pId);
+//	if (!hProc)
+//	{
+//        perror("Open process failed");
+//		printf("error 2");
+//		return 0;
+//	}
+//
+//    // proc baseaddress after lodaing to the RAM. probably 0x400000.
+//	DWORD_PTR baseAddress = (DWORD)getModuleHandle(hProc);
+//    printf("proc start in this address: %d \n", baseAddress);
+//
+//    // The pointer to player struct is a static address.
+//    // always be in the same relative place to proc base address
+//    DWORD_PTR pPlayer = baseAddress + PLAYER_RELATIVE_ADDRESS;
+//    printf("Player address is: %d \n", pPlayer);
+//
+//    // Get player health location
+//    PVOID pHealth = 0;
+//    int is_valid = ReadProcessMemory(hProc, (LPCVOID)pPlayer, &pHealth, sizeof(DWORD), NULL);
+//    if (!is_valid)
+//    {
+//        printf("error 3");
+//        return 0;
+//    }
+//    // pHealth now got the address of the player.
+//    pHealth = PVOID(DWORD(pHealth) + HEALTH_OFFSET);
+//
+//    // Get player health.
+//    printf("player health address is: %d \n", (DWORD)pHealth);
+//    is_valid = ReadProcessMemory(hProc, pHealth, &playerHealth, sizeof(int), NULL);
+//    if (!is_valid)
+//    {
+//        printf("error 4");
+//        return 0;
+//    }
+//    printf("Player current health is %d \n", playerHealth);
+//
+//    //Change player health.
+//    WriteProcessMemory(hProc, pHealth, &NEW_PLAYER_HEALTH, sizeof(int), NULL);
+//}
+//
+//DWORD_PTR getModuleHandle(HANDLE processHandle)
+//{
+//    // TODO: undresatnd code.
+//    DWORD_PTR   baseAddress = 0;
+//    HMODULE* moduleArray;
+//    LPBYTE      moduleArrayBytes;
+//    DWORD       bytesRequired;
+//
+//    if (EnumProcessModules(processHandle, NULL, 0, &bytesRequired))
+//    {
+//        if (bytesRequired)
+//        {
+//            moduleArrayBytes = (LPBYTE)LocalAlloc(LPTR, bytesRequired);
+//
+//            if (moduleArrayBytes)
+//            {
+//                unsigned int moduleCount;
+//
+//                moduleCount = bytesRequired / sizeof(HMODULE);
+//                moduleArray = (HMODULE*)moduleArrayBytes;
+//
+//                if (EnumProcessModules(processHandle, moduleArray, bytesRequired, &bytesRequired))
+//                {
+//                    baseAddress = (DWORD_PTR)moduleArray[0];
+//                }
+//
+//                LocalFree(moduleArrayBytes);
+//            }
+//        }
+//    }
+//    return baseAddress; 
+//}
