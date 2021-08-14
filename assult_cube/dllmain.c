68e0bd5920150b881d5029a3377c1897cb2c449c
Guy Krinsky <guykrinsky9@gmail.com>
2021-08-14 17:04:15 +0300

    created a god mode with option to teleport to enemy player

--- assult_cube/dllmain.c
+++ assult_cube/dllmain.c
@@ -5,10 +5,55 @@
 #include "mem.h"
 #include <stdlib.h>
 
+typedef unsigned char _BYTE;
+
+
+struct player_struct
+{
+    char padding[4];
+    int head_x_value;
+    int head_y_value;
+    int head_z_value;
+    char padding2[36];
+    int x_value;
+    int y_value;
+    int z_value;
+    int yaw_angel;
+    int pitch_angel;
+    _BYTE gap48[176];
+    int health;
+    int shield;
+    _BYTE gap100[292];
+    char is_shooting;
+    char name[10];
+    _BYTE gap22F[253];
+    int team;
+    _BYTE gap330[68];
+    struct weapon_struct* pointer_to_current_weapon;
+    _BYTE gap378[73];
+    char field_3D;
+};
+
+
+struct weapon_struct
+{
+    _BYTE gap0[4];
+    int type;
+    struct player_struct* owner;
+    int* type_and_data;
+    int* mag_ammo;
+    int* ammo;
+};
+
+
+typedef struct player_struct player_t;
+typedef struct weapon_struct ammo;
 
 // This is a relative address to the process base pointer.
 // From there the program read the address of the reciol funciton.
-#define PLAYER_RELATIVE_ADDRESS_POINTER 0x10F4F4
+#define PLAYER_POINTER_RELATIVE_ADDRESS 0x10F4F4
+#define OTHER_PLAYERS_ARRAY_RELATIVE_ADDRESS 0x10F4F8
+#define NUMBER_OF_PLAYERS_RELATIVE_ADDRESS 0x10F500
 #define HEALTH_OFFSET 0xF8
 
 //TODO: change to real function length.
@@ -41,17 +86,23 @@ VOID mainHack()
     int proc_id = get_process_id(WINDOW_NAME);
     HANDLE process_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proc_id);
     DWORD process_base_address = get_process_base_address(process_handle);
+
+    player_t* player_pointer = *(player_t**)(process_base_address + PLAYER_POINTER_RELATIVE_ADDRESS);
+    DWORD number_of_players = *(DWORD*)(NUMBER_OF_PLAYERS_RELATIVE_ADDRESS + process_base_address);
+    player_t**  other_players = process_base_address + OTHER_PLAYERS_ARRAY_RELATIVE_ADDRESS;
     
     if (!cancel_reciol(process_handle, process_base_address))
         msg_box("error canceling reciol");
 
     PVOID inject_address = (PVOID)(process_base_address + INJECT_ADDRESS_MAP_HACK);
-    
     DWORD process_status = 1;
+    player_t* enemy_player;
+    DWORD counter = 1;
     while (GetExitCodeProcess(process_handle, &process_status) && process_status == STILL_ACTIVE)
     {
-        if (GetAsyncKeyState(VK_RBUTTON) & 1)
+        if (GetAsyncKeyState(VK_NUMPAD9) & 1)
         {
+
             if (!is_hacked)
             {
                 replace_code_with_nop(process_handle, inject_address, old_bytes, BYTES_COUNT);
@@ -61,6 +112,45 @@ VOID mainHack()
                 patch_bytes(process_handle, inject_address, old_bytes, BYTES_COUNT);
             }
             is_hacked = !is_hacked;
+        }
+        
+        if (GetAsyncKeyState(VK_NUMPAD0) & 1)
+        {
+            number_of_players = *(DWORD*)(NUMBER_OF_PLAYERS_RELATIVE_ADDRESS + process_base_address);
+            if (number_of_players == 0)
+                continue;
+            do
+            {
+                enemy_player = *((player_t**)(*other_players)+counter);
+                if (enemy_player && enemy_player->team != player_pointer->team)
+                    break;
+                counter++;
+            } while (counter < number_of_players);
+            
+            if (!enemy_player)
+                // didn't find enemy player.
+                continue;
+
+            player_pointer->x_value = enemy_player->x_value + 5;
+            player_pointer->y_value = enemy_player->y_value;
+            player_pointer->z_value = enemy_player->z_value;
+            
+        }
+
+
+        if (player_pointer->health < 200)
+        {
+            player_pointer->health = 234;
+        }
+
+        if (player_pointer->shield < 100)
+        {
+            player_pointer->shield = 122;
+        }
+
+        if (*(player_pointer->pointer_to_current_weapon->ammo) < 50)
+        {
+            *(player_pointer->pointer_to_current_weapon->ammo) = 50;
         }
     }
 }
@@ -153,7 +243,7 @@ void __declspec(naked) increase_player_health()
     {
         push ebx
 
-        mov ebx, PLAYER_RELATIVE_ADDRESS_POINTER
+        mov ebx, PLAYER_POINTER_RELATIVE_ADDRESS
         add ebx, 0x400000
         mov ebx, [ebx]
         add ebx, HEALTH_OFFSET
